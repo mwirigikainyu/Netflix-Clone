e9fd540f482ed8d4c131872a1f96d10c1d7a18cd
mwirigikainyu <mwirigikainyu@gmail.com>
2021-06-10 18:28:45 +0300

    Uninstalled empty dependencies

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div style={{ paddingTop: "20px" }}>
+    <div style={{ paddingTop: "20px", textAlign: "center" }}>
       <p>Built by Michelle Kainyu</p>
     </div>
   );
